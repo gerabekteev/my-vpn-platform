88b6c607d3c4f65104a40b38b2a5e2698e7186e4
gerabekteev <gerabekteev@yandex.ru>
2025-07-11 19:18:24 +0300

    без проверки домменного имени

--- app/outline_api.py
+++ app/outline_api.py
@@ -1,15 +1,29 @@
+import ssl
+from urllib3.poolmanager import PoolManager
+from requests.adapters import HTTPAdapter
 import requests
-from urllib.parse import urlparse, parse_qs
+
+class SSLIgnoreHostnameAdapter(HTTPAdapter):
+    def init_poolmanager(self, *args, **kwargs):
+        ctx = ssl.create_default_context(cafile="/root/my-vpn-platform/outline.crt")
+        ctx.check_hostname = False  # Отключаем проверку имени хоста
+        kwargs['ssl_context'] = ctx
+        self.poolmanager = PoolManager(*args, **kwargs)
 
 class OutlineServer:
-    def __init__(self, access_url: str):
+    def __init__(self, access_url):
+        from urllib.parse import urlparse, parse_qs
         parsed = urlparse(access_url)
-        self.base_url = f"{parsed.scheme}://{parsed.hostname}:{parsed.port}/access-keys"
         self.token = parse_qs(parsed.query)['token'][0]
-        self.headers = {'Authorization': f'Bearer {self.token}'}
+        self.base_url = f"{parsed.scheme}://{parsed.hostname}:{parsed.port}/access-keys"
+        self.headers = {'Authorization': f"Bearer {self.token}"}
+        
+        # Настроим сессию с кастомным SSL-адаптером
+        self.session = requests.Session()
+        self.session.mount("https://", SSLIgnoreHostnameAdapter())
 
     def create_key(self, name: str):
-        r = requests.post(
+        r = self.session.post(
             self.base_url,
             headers=self.headers,
             json={'name': name},
